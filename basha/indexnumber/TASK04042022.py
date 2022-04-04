6ee3b1957b279b0c82cc690ab1aef09bbf19ef56
basha <mahaboobbashashaik1998@gmail.com>
2022-04-04 18:19:35 +0530

    DONE:04042022

--- basha/indexnumber/TASK04042022.py
+++ basha/indexnumber/TASK04042022.py
@@ -1,20 +1,7 @@
 import pandas as pd
 import warnings
-
 warnings.filterwarnings('ignore')
-data = pd.read_csv('2.csv')
-data_twenty = data.head(30)
-data_twenty.drop('Unnamed: 0', inplace=True, axis=1)
-data_twenty['new_col'][1]
-data_twenty['new_col'][2]
-data_twenty.loc[::2, 'new_col'] = 'CHUBB QUICK FLOOD ESTIMATE WORKSHEET (@) PERSONAL (PRIMARY) FLOOD action:Updated'
-data_twenty.loc[::3, 'new_col'] = 'Epoch:40'
-data_twenty.loc[::2,
-'new_col'] = 'CHUBB QUICK FLOOD ESTIMATE WORKSHEET (@) PERSONAL (PRIMARY) FLOOD action:Updated,Package:libcacard-2.7.0-1.el7.x86_64'
-data_twenty['new_col'][9]
-df = pd.DataFrame(data_twenty)
-df.to_csv('Samplefile', encoding='cp1252')
-
+df=pd.read_csv('samplefile.csv',encoding='utf-8')
 
 # using constructor
 
